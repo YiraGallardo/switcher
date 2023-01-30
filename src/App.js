afa7f6d9a46cd04d497d1d1e3214f29190758589
Yira Mac <yiramac@Yiras-MacBook-Pro.local>
2023-01-29 19:35:40 -0600

    controls

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./styles.css";
 
-function App() {
+export const App = () => {
+  const handleToggled = () => {
+    document.body.classList.toggle("dark");
+    localStorage.setItem("dark", document.body.className);
+  };
+  useEffect(() => {
+    if (localStorage.getItem("dark")) {
+      document.body.classList.add("dark");
+    }
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <label>
+        <input type="checkbox" onClick={handleToggled} />
+        <span className="switch">
+          <span className="handle" />
+        </span>
+      </label>
     </div>
   );
-}
+};
 
 export default App;
